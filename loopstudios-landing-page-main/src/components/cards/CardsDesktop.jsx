6ae64d5007baf1e7b47554013b92475fe22ae9e4
rahmanwghazi <rahmanwghazi@student.telkomuniversity.ac.id>
2022-03-02 23:09:20 +0700

    fix desktop-card desc

--- loopstudios-landing-page-main/src/components/cards/CardsDesktop.jsx
+++ loopstudios-landing-page-main/src/components/cards/CardsDesktop.jsx
@@ -18,29 +18,38 @@ const CardsDesktop = () => {
                 </div>
             </div>
             <section>
+            <article>
                 <img src={deepImg} alt="" />
                 <h3>{'Deep Earth'}</h3>
-
+            </article>
+            <article>
                 <img src={nightImg} alt="" />
                 <h3>{'Night Arcade'}</h3>
-
+            </article>
+            <article>
                 <img src={socImg} alt="" />
                 <h3>{'Soccer Team VR'}</h3>
-
+            </article>
+            <article>
                 <img src={gridImg} alt="" />
                 <h3>{'The Grid'}</h3>
-
+            </article>
+            <article>
                 <img src={aboveImg} alt="" />
                 <h3>{'From Up Above VR'}</h3>
-
+            </article>
+            <article>
                 <img src={pocketImg} alt="" />
                 <h3>{'Pocket borealis'}</h3>
-
+            </article>
+            <article>
                 <img src={curImg} alt="" />
                 <h3>{'The Curiosity'}</h3>
-
+            </article>
+            <article>
                 <img src={fishImg} alt="" />
                 <h3>{'Make It Fisheye'}</h3>
+            </article>
             </section>
         </section>
     )
